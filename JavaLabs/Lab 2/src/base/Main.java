b9f06ea740245464e7067296d48d30da4912a992
Aaron Michael Kippins <AKippins@hotmail.com>
2022-01-27 14:54:57 -0500

    Updating and dropping in lesson and lab 2.

---
+++ JavaLabs/Lab 2/src/base/Main.java
@@ -0,0 +1,23 @@
+package base;
+
+public class Main {
+    /**
+     * # Lab Two
+     *
+     * Ok for this lab we're going to reiterate a lot of the things that we went over in class.
+     *
+     * Our Goals are:
+     * - Conditionals
+     *     - If
+     *     - Else
+     *     - Else If
+     */
+    public static void main(String[] args) {
+        // Make an if statement that triggers a print or println statement
+        // Make an if, else statement where the else statement triggers a print or println statement
+        // Make an if, else if, else statement where the else if statement triggers a print or println statement
+        // Make 2 variables and use them in an if else conditional print from one of the sections
+        // Make an if statement using 2 variables and an AND(&&) statement that triggers a print or println statement
+        // Make an if statement using 2 variables and an OR(||) statement that triggers a print or println statement
+    }
+}
