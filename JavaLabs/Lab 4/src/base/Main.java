39d6090cd82795a7755c207c308b276ca678f190
Aaron Michael Kippins <AKippins@hotmail.com>
2022-02-10 11:31:56 -0500

    Lesson 4 and Lab 4

---
+++ JavaLabs/Lab 4/src/base/Main.java
@@ -0,0 +1,63 @@
+package base;
+
+public class Main {
+    /**
+     * # Lab Four
+     * ---
+     *
+     * For this lab we're going to be making and using a bunch of functions.
+     *
+     * Our Goals are:
+     * - Switch Case
+     * - Looping
+     * - Making our own functions
+     * - Combining functions
+     * - Structuring solutions
+     */
+    public static void main(String[] args) {
+        // Give me an example of you using switch case.
+        // Give me an example of you using a for loop
+        // Give me an example of you using a while loop
+    }
+    // For the following todos you'll probably want to define them below and then call them from the main function above.
+
+    /**
+     * I want you to write a function that will take in a number and raise it to the power given.
+     * For example if given the numbers 2 and 3. The math that the function should do is 2^3
+     * and should print out or return 8. Print the output.
+     */
+
+    /**
+     * I want you to write a function that will take in a list and see how many times a given number is in the list.
+     * For example if the array given is [2,3,5,2,3,6,7,8,2] and the number given is 2 the function should print out
+     * or return 3. Print the output.
+     */
+
+    /**
+     * Create a function named summation that will take in an int[ ] and return an int.
+     * I want the function to then go through the given array and sum up digits in the array.
+     * Once that’s done print or return the answer. Print the output
+     */
+
+    /**
+     * Give me a function that gives the answer to the pythagorean theorem.
+     * I'd like you to reuse the exponent function from above as well as the functions below to make your function.
+     *
+     * If you don't remember the pythagorean theorem the formula is (a^2 + b^2 = c^2).
+     * Given a and b as parameters i'd like you to return c.
+     *
+     * If this doesn't make sense look up `Pythagorean Theorem Formula` on google.
+     *
+     * Once that’s done print or return the answer. Print the output
+     */
+
+    int addition(int a, int b) {
+        int answer = a + b;
+        return answer;
+    }
+
+    int division(int a, int b) {
+        int answer = a / b;
+        return answer;
+    }
+}
